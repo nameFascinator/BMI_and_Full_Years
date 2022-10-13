6d569c754d6106305e508a2cfec77c20f19b302f
Viktor <V.V.Chmilenko@googlemail.com>
2022-10-13 11:20:13 +0200

    def count_Age

--- main.py
+++ main.py
@@ -56,3 +56,55 @@ class BMI_and_Full_Years(QtWidgets.QWidget):
                 dlg.setStyleSheet("background-color: rgb(180, 3, 3);")
                 button = dlg.exec()
                 QMessageBox.StandardButton.Ok
+
+    def count_Age(self):
+        now = date.today()
+        year = self.ui.textEdit_Year_Input.toPlainText()
+        month = self.ui.textEdit_Monthr_Input.toPlainText()
+        day = self.ui.textEdit_Day_Input.toPlainText()
+
+        if (len(year) or len(month) or len(day)) > 0:
+            try:
+                birthdate = date(int(year), int(month), int(day))
+                age_in_years = (now - birthdate) / 365.2425
+                age_in_years = str(age_in_years)
+                age_in_years = age_in_years[:3]
+                if int(age_in_years) < 150:
+                    self.ui.label_Years_Output.setText(f"Full Years is:      {age_in_years}")
+                else:
+                    dlg = QMessageBox(self)
+                    dlg.setWindowTitle("???????????????????????????????????????")
+                    dlg.setText(f"What!? you are {int(age_in_years)} years old!? I can not believe you!")
+                    dlg.setIcon(QMessageBox.Icon.Question)
+                    dlg.setStyleSheet("background-color: rgb(0, 255, 15);")
+                    button = dlg.exec()
+                    QMessageBox.StandardButton.Ok
+
+            except:
+                try:
+                    if (len(year) or len(month) or len(day)) > 0 and (len(day) and len(month)) < 3 and ((len(day) and len(month)) < 2 and int(month) < 12 and int(month) > 1 and int(day) < 31 and int(day) > 1):
+                        dlg = QMessageBox(self)
+                        dlg.setWindowTitle("ERROR")
+                        dlg.setText("Input should be integer or float number")
+                        dlg.setIcon(QMessageBox.Icon.Warning)
+                        dlg.setStyleSheet("background-color: rgb(180, 3, 3);")
+                        button = dlg.exec()
+                        QMessageBox.StandardButton.Ok
+                    elif (len(day) or len(month)) > 2 or int(month) > 12 or int(month) < 1 or int(day) > 31 or int(day) < 1:
+                        dlg = QMessageBox(self)
+                        dlg.setWindowTitle("ERROR")
+                        dlg.setText("Month or Day incorrect")
+                        dlg.setIcon(QMessageBox.Icon.Warning)
+                        dlg.setStyleSheet("background-color: rgb(58, 169, 255);")
+                        button = dlg.exec()
+                        QMessageBox.StandardButton.Ok
+                except:
+                    dlg = QMessageBox(self)
+                    dlg.setWindowTitle("ERROR")
+                    dlg.setText("Input should be integer or float number")
+                    dlg.setIcon(QMessageBox.Icon.Warning)
+                    dlg.setStyleSheet("background-color: rgb(180, 3, 3);")
+                    button = dlg.exec()
+                    QMessageBox.StandardButton.Ok
+        else:
+            pass
